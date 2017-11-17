ad4d343e6cad2468c28f0787a7d0ff0058c324fb
dckristiono <dckristiono@gmail.com>
2017-11-17 08:23:53 +0700

    add function
    add show_my_absence

--- api/controllers/absenceControllers.js
+++ api/controllers/absenceControllers.js
@@ -39,3 +39,59 @@ exports.create_a_absence = function (req, res) {
       }
 }
 
+exports.show_my_absence = function (req, res){
+  var current_page = 1, prev_page = null, prev_page_url = null, per_page = 3
+    if (typeof req.query.page === 'undefined') {
+      current_page = 1
+    } else {
+      current_page = req.query.page
+      if(current_page > 1) {
+        prev_page = current_page-1
+      }
+    }
+
+    if(prev_page !== null){
+      prev_page_url = config_http.url+'/absence/me?page='+prev_page
+    } 
+
+    if (typeof req.query.per_page  !== 'undefined') {
+      per_page = parseInt(req.query.per_page)
+    }
+  Absence.find({
+    user_id: req.user.id
+  })
+  .count(function (err, count){
+    Absence.find({
+    user_id: req.user.id
+    })
+    .skip(per_page*(current_page-1))
+    .limit(per_page)
+    .exec(function (err, absence) {
+      if (err) throw err;
+      var output = {},
+          total = count,
+          last_page = ((total-(total%per_page))/per_page)+1,
+          next_page_url = config_http.url+'/absence/me?page='+(current_page+1),
+          from = (per_page*(current_page-1))+1,
+          data = absence
+
+      var to = (per_page*current_page)
+      if (total<to) {
+        to = total
+      }
+
+      output.total = total
+      output.per_page = per_page
+      output.current_page = current_page
+      output.last_page = last_page
+      output.next_page_url = next_page_url
+      output.prev_page_url = prev_page_url
+      output.from = from
+      output.to = to
+      output.data = data
+
+      res.send(output)
+    })
+  })
+  
+}
